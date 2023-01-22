4684c781516020ac3ffc13873d2b41753e54cc9d
Fathiya Barnawi <fofror-1414@outlook.sa>
2023-01-22 23:38:52 +0300

    first proj

---
+++ script.js
@@ -0,0 +1,7 @@
+function information (firstname , lastname , age){
+    console.log("Hello dir :" , firstname , lastname , age)
+}
+
+information("FATHIYYA :" , "MALIK :" , 20);
+information("RAWYAH :" , "HASAN :" , 22);
+information("SAIYDH :" , "BUKAR:" , 15);
